d772754e92b44446a98fbcc5c4896a58382e9414
padmam <padmam@users.noreply.github.com>
2016-11-17 15:58:08 +0530

    change

--- api.sh
+++ api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl 'http://localhost:8153/go/api/pipelines/B-smoke/schedule'  -u 'padma:Password@19'  -H 'Confirm: true' -X POST -d '{materials[git_material]=git rev-parse origin/master}'
+curl 'http://localhost:8153/go/api/pipelines/B-smoke/schedule'  -u 'padma:Password@19'  -H 'Confirm: true' -X POST -d '{materials[git_material]=597a7a36a84e33f03e75334c06ca39a79a9d9f80}'
