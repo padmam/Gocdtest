cb4a25605c84111061ed49f83547e9c88d79cedb
padmam <padmam@users.noreply.github.com>
2016-11-17 16:12:10 +0530

    commit 3

--- api.sh
+++ api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl 'http://localhost:8153/go/api/pipelines/B-smoke/schedule'  -u 'padma:Password@19'  -H 'Confirm: true' -X POST -d '{materials[git_material]=597a7a36a84e33f03e75334c06ca39a79a9d9f80}'
+curl 'http://localhost:8153/go/api/pipelines/B-smoke/schedule'  -u 'padma:Password@19'  -H 'Confirm: true' -X POST -d '{materials[git]=9f1075e3debedea49eb0ed0678baf2959ac78098}'
